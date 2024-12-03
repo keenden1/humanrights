847fad59d06d1abf3b28e89b974fd699a76a54d5
kenfrianeza-dev <kenfrianeza.dev@gmail.com>
2024-12-04 03:04:04 +0800

    Downloadable Booklet v1.0.5
    Changes:
    - Added functionality to allow booklets to be downloaded and viewed.

--- resources/views/main/lawbook.blade.php
+++ resources/views/main/lawbook.blade.php
@@ -22,39 +22,39 @@
 </div>
 <div id="Box">
     <div class="box-main">
-            <div class="box-box"> 
+            <a href="https://pcw.gov.ph/assets/files/2020/09/RA-7877.pdf" target="_blank" rel="noopener noreferrer" class="box-box">
                 <div class="picture"><img src="{{ asset('image/sample.png') }}" alt="book" ></div>
                 <div class="text">
                 <h1>REPUBLIC ACT 7877</h1>
-                <p>Anti-Sexual Harrassment Act of 1995</p>
+                <p>Republic Act 7877, also known as the Anti-Sexual Harassment Act of 1995, is a law in the Philippines that protects people from sexual harassment in the workplace, educational institutions, and public spaces. The law aims to uphold the dignity of individuals and guarantee respect for human rights.</p>
                 </div>
-            </div>
+            </a>
 
-            <div class="box-box-2"> 
+            <a href="https://library.pcw.gov.ph/wp-content/uploads/2024/09/PCW-Brochure-R.A-9262-Anti-VAWC-Act-English-2024.pdf" target="_blank" rel="noopener noreferrer" class="box-box-2">
                 <div class="picture-2"><img src="{{ asset('image/sample1.png') }}" alt="book" ></div>
                 <div class="text-2">
-                <h1>REPUBLIC ACT 9208</h1>
-                <p>The Anti-Trafficking in Persons Act of 2003, as amended by the Expanded Anti-Trafficking in Person Act of 2013</p>
+                <h1>REPUBLIC ACT 9262</h1>
+                <p>Republic Act (RA) 9262, also known as the Anti-Violence Against Women and their Children Act of 2004, is a law that addresses violence against women and children (VAWC)</p>
                 </div>
-            </div>
+            </a>
     </div>
     <br><br>
     <div class="box-main">
-            <div class="box-box"> 
-                <div class="picture"><img src="{{ asset('image/sample.png') }}" alt="book" ></div>
+            <a href="https://library.pcw.gov.ph/wp-content/uploads/2022/09/PCW-Brochure-R.A-11210-Expanded-Maternity-Leave-in-English-2024.pdf" class="box-box">
+                <div class="picture"><img src="{{ asset('image/ra-11210.png') }}" alt="book" ></div>
                 <div class="text">
-                <h1>REPUBLIC ACT 7877</h1>
-                <p>Anti-Sexual Harrassment Act of 1995</p>
+                <h1>REPUBLIC ACT 11210</h1>
+                <p>The 105-Day Expanded Maternity Leave Law (RA 11210 or EML) provides the updated policy on maternity leave that covers females who are workers in the private and public sectors, workers in the informal economy, voluntary contributors to the Social Security System (SSS), and national athletes.</p>
                 </div>
-            </div>
+            </a>
 
-            <div class="box-box-2"> 
-                <div class="picture-2"><img src="{{ asset('image/sample1.png') }}" alt="book" ></div>
+            <a href="https://library.pcw.gov.ph/wp-content/uploads/2024/09/PCW-Brochure-R.A-11313-Safe-Spaces-Act-English-2024.pdf" class="box-box-2">
+                <div class="picture-2"><img src="{{ asset('image/ra-11313.png') }}" alt="book" ></div>
                 <div class="text-2">
-                <h1>REPUBLIC ACT 9208</h1>
-                <p>The Anti-Trafficking in Persons Act of 2003, as amended by the Expanded Anti-Trafficking in Person Act of 2013</p>
+                <h1>REPUBLIC ACT 11313</h1>
+                <p>11313 (known as the “Safe Spaces Act”) with the aim of ensuring the equality, security, and safety of every individual, in both private and public spaces, including online and in workplaces.</p>
                 </div>
-            </div>
+            </a>
     </div>
 </div>
 
