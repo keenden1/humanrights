baac398ddee706be7cc9a1bf6d257deb86dca6eb
Ian Beguas <ianbeguas@gmail.com>
2024-12-15 08:42:56 +0800

    officer  - user account details button

--- resources/views/layout/officerheader.blade.php
+++ resources/views/layout/officerheader.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>@yield('title', 'Officer Dashboard')</title>
+    @yield('styles')
     <link rel="icon" type="image/x-icon" href="logo/logo.png">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/admin/newdashboard.css') }}">
     <link rel="stylesheet" type="text/css" href="{{ asset('css/officer/newstyle.css') }}">
